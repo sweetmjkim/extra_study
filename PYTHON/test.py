94960e1dfabccb4c2440e807f416ab70cc1c163f
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-15 06:07:58 +0900

    1

---
+++ PYTHON/test.py
@@ -0,0 +1,16 @@
+food = "Python's favorite food is perl"
+print(food)
+
+say = '"Python is very easy." he says.'
+print(say)
+
+a = "Life is too short"
+print(len(a))
+
+a = "Life is too short, You need Python"
+print(a[3])
+
+a = "Life is too short, You need Python"
+b = a[0] + a[1] + a[2] + a[3]
+print(b)
+
